dc5d844c0e6a09d9645ff231ce88c7952476b139
AnaMithie <mithieana@gmail.com>
2025-02-04 11:48:37 -0300

    lista 4

---
+++ if/lista 4/list.js
@@ -0,0 +1,60 @@
+let idade = prompt("digite sua idade:")
+if (idade <= 12) {console.log ("Criança")}
+else if (idade <=17){console.log("Adolecente")}
+else if (idade <= 60){console.log("Adulto")}
+else if (idade > 60){console.log("Idoso")}
+console.log("-------------------------------------------")
+
+let temperatura = prompt("digite a temperatura:")
+if (temperatura <= 10) {console.log ("Muito frio")}
+else if (temperatura <=20){console.log("Frio")}
+else if (temperatura <= 30){console.log("Agradável")}
+else if (temperatura > 30){console.log("Muito quente")}
+console.log("-------------------------------------------")
+
+let turno = prompt("digite a sigla do seu turno:")
+if (turno = M) {console.log ("Bom dia!")}
+else if (turno = V){console.log("Boa tarde!")}
+else if (turno = N){console.log("boa noite!")}
+else {console.log("Turno inválido")}
+console.log("-------------------------------------------")
+
+let desconto = prompt("digite o valor da compra:")
+if (desconto == 100) {console.log ("5% de desconto")}
+else if (desconto <=199){console.log("10% de desconto")}
+else if (desconto >= 200){console.log("15% de desconto")}
+console.log("-------------------------------------------")
+
+
+let nota = prompt("Digite a nota:")
+if (nota >= 90) {console.log ("Nota A")}
+else if (nota >= 80){console.log("Nota B")}
+else if (nota >= 70){console.log("Nota C")}
+else if (nota >= 60){console.log("Nota D")}
+else if (nota <= 50){console.log("Nota F")}
+console.log("-------------------------------------------")
+
+let dia = prompt("digite um numero de 1 a 7:")
+if (dia = 1){console.log("Domingo")}
+else if (dia = 2){console.log("Segunda-feira")}
+else if (dia = 3){console.log("Terça-feira")}
+else if (dia = 4){console.log("Quarta-feira")}
+else if (dia = 5){console.log("Quinta-feira")}
+else if (dia = 6){console.log("Sexta-feira")}
+else if (dia = 7){console.log("Sábado")}
+else {console.log("Digite o número somente de 1 a 7!")}
+console.log("-------------------------------------------")
+
+let distancia = prompt("Digite a distância:")
+if (distancia <= 400) {console.log ("Pista curta")}
+else if (distancia <= 800){console.log("Pista média")}
+else if (distancia <= 1500){console.log("Pista longa")}
+else if (distancia > 1500){console.log("Pista muito longa")}
+console.log("-------------------------------------------")
+
+let jogador = prompt("Digite sua pontuação:")
+if (jogador <= 1000) {console.log ("Iniciante")}
+else if (jogador <= 5000){console.log("Intermediário")}
+else if (jogador <= 10000){console.log("Avançado")}
+else if (jogador > 10000){console.log("Mestre")}
+console.log("-------------------------------------------")
