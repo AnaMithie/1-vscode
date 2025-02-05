21b800da6f2b615773515b8aa37145989f0590c9
AnaMithie <mithieana@gmail.com>
2025-02-05 10:35:32 -0300

    lista 55

---
+++ e ou/test.js
@@ -0,0 +1,7 @@
+let valor = 150 
+let clienteVip = false
+if(valor >= 100 || clienteVip == true) {
+    console.log("Vôce ganhou frete grátis")
+} else {
+    console.log("Tem que pagar o frete")
+}
